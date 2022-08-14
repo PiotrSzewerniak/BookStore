479298175fb8d0cc1ebeed31301d8c3e80e4bd57
Wassper <piotr.szewerniak267@gmail.com>
2022-08-15 00:29:15 +0200

    Methods: adding book, constructor, destructor, showing book

--- book.cpp
+++ book.cpp
@@ -1,5 +1,38 @@
+#include "book.h"
 #include <iostream>
 #include <string.h>
-#include <stdlib.h>
 
 using namespace std;
+
+void Book::addbook()
+{
+    cout<<"Enter author"<<endl;
+    getline(cin, author);
+    cout<<"Enter title"<<endl;
+    getline(cin, title);
+    cout<<"Enter price"<<endl;
+    cin>>price;
+    cout<<"Enter amount in store"<<endl;
+    cin>>inStore;
+}
+
+Book::Book(string a, string t, float p, int s)
+{
+    author = a;
+    title = t;
+    price = p;
+    inStore = s;
+}
+
+Book::~Book()
+{
+
+}
+
+void Book::showentry()
+{
+    cout<<"Author: "<<author<<endl;
+    cout<<"Title: "<<title<<endl;
+    cout<<"Price: "<<price<<endl;
+    cout<<"Amount in store: "<<inStore<<endl;
+}
