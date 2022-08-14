27f8502dc272bcad09c84b6d0288ae683950dd95
Wassper <piotr.szewerniak267@gmail.com>
2022-08-14 22:09:27 +0200

    Header file with contents

--- book.h
+++ book.h
@@ -1,8 +1,21 @@
-#include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Book
 {
+    private:
 
+    vector<char> *author = new vector<char>(30);
+    vector<char> *title = new vector<char>(30);
+    float price;
+    int inStore;
+
+    public:
+
+    void addbook();
+    void editentry();
+    void showentry();
+    void buybook();
+    int lookfor(vector<char> v1, vector<char> v2);
 };
